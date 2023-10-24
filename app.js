20cfb5f1dcb122c0726c1f4ccebf0f01e4de6a99
Shubham-Srivastava11 <ssrivastava2297@gmail.com>
2023-10-24 21:36:58 +0530

    updated query

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
-const { CONFIG } = require("./config");
 
 const app = express();
 const port = 3000;
@@ -18,7 +17,6 @@ const pool = mysql.createPool({
 
 // Create a new product in inventory
 app.post("/create-product", (req, res) => {
-  console.log(req);
   const { productName, productCategory, productDesc, productRating } = req.body;
 
   if (!productName || !productDesc) {
@@ -28,50 +26,86 @@ app.post("/create-product", (req, res) => {
   }
 
   pool.query(
-    "INSERT INTO productDetails ( productName, productCategory, productDesc, productRating) VALUES (?, ?, ?, ?)",
-    [productName, productCategory, productDesc, productRating],
+    `SELECT * FROM category WHERE categoryName = ?`,
+    [productCategory],
     (err, result) => {
-      if (err) {
-        console.error("Error during registration:", err);
-        return res
-          .status(500)
-          .json({ status: 500, message: "Failed to register user." });
-      }
+      if (result.length === 0) {
+        pool.query(
+          "INSERT INTO category (categoryName) VALUES (?)",
+          [productCategory],
+          (err, result1) => {
+            if (err) {
+              console.error("Error during category creation:", err);
+              return res
+                .status(500)
+                .json({ status: 500, message: "Failed to create category." });
+            }
+            pool.query(
+              "INSERT INTO productDetails ( productName, productDesc, productRating, categoryId) VALUES (?, ?, ?, ?)",
+              [productName, productDesc, productRating, result1.insertId],
+              (err, result) => {
+                if (err) {
+                  console.error("Error during product creation:", err);
+                  return res
+                    .status(500)
+                    .json({ status: 500, message: "Failed to Add product." });
+                }
 
-      return res.status(201).json({
-        status: 201,
-        message: `Created a new product - ${productName}, successfully.`,
-      });
+                return res.status(201).json({
+                  status: 201,
+                  message: `Created a new product - ${productName}, successfully.`,
+                });
+              }
+            );
+          }
+        );
+      } else {
+        if (err) {
+          console.error("Error during fetching category", err);
+          return res
+            .status(500)
+            .json({ status: 500, message: "Failed to fetch category." });
+        }
+        pool.query(
+          "INSERT INTO productDetails ( productName, productDesc, productRating, categoryId) VALUES (?, ?, ?, ?)",
+          [productName, productDesc, productRating, result[0].categoryId],
+          (err, result) => {
+            if (err) {
+              console.error("Error during product creation:", err);
+              return res
+                .status(500)
+                .json({ status: 500, message: "Failed to Add product." });
+            }
+
+            return res.status(201).json({
+              status: 201,
+              message: `Created a new product - ${productName}, successfully.`,
+            });
+          }
+        );
+      }
     }
   );
 });
 
 // Update details for an existing Product
 app.put("/update-product/:id", (req, res) => {
-  const { productName, productCategory, productDesc, productRating } = req.body;
+  const { productName, categoryId, productDesc, productRating } = req.body;
   const { id } = req.params;
 
   if (!id) {
-    return res
-      .status(400)
-      .json({ status: 400, message: "Email and password are required." });
+    return res.status(400).json({ status: 400, message: "Id is required." });
   }
 
   pool.query(
-    "UPDATE productDetails SET productName = ?, productCategory = ?, productDesc = ?, productRating = ?  WHERE productId = ?",
-    [productName, productCategory, productDesc, productRating, id],
+    "UPDATE productDetails SET productName = ?, productDesc = ?, productRating = ?, categoryId = ?  WHERE productId = ?",
+    [productName, productDesc, productRating, categoryId, id],
     (err, result) => {
       if (err) {
-        console.error("Error during login:", err);
+        console.error("Error during update product details:", err);
         return res
           .status(500)
-          .json({ status: 500, message: "Failed to authenticate user." });
-      }
-
-      if (result.length === 0) {
-        return res
-          .status(401)
-          .json({ status: 401, message: "Invalid credentials." });
+          .json({ status: 500, message: "Failed to update product." });
       }
 
       return res
@@ -83,22 +117,28 @@ app.put("/update-product/:id", (req, res) => {
 
 // Get all listed products
 app.get("/products", (req, res) => {
-  pool.query("SELECT * FROM productDetails", (err, result) => {
-    if (err) {
-      console.error("Error fetching product details:", err);
-      return res
-        .status(500)
-        .json({ status: 500, message: "Failed to fetch product details." });
-    }
+  pool.query(
+    `select productdetails.productId, productName, category.categoryName, productDesc, productRating, stockQuantity 
+    from productdetails 
+    join category on productdetails.categoryId = category.categoryId
+    left join inventory on inventory.productId = productdetails.productId`,
+    (err, result) => {
+      if (err) {
+        console.error("Error fetching product details:", err);
+        return res
+          .status(500)
+          .json({ status: 500, message: "Failed to fetch product details." });
+      }
 
-    if (result.length === 0) {
-      return res
-        .status(404)
-        .json({ status: 404, message: `Products not found.` });
-    }
+      if (result.length === 0) {
+        return res
+          .status(404)
+          .json({ status: 404, message: `Products not found.` });
+      }
 
-    return res.status(200).json(result);
-  });
+      return res.status(200).json(result);
+    }
+  );
 });
 
 app.listen(port, () => {
